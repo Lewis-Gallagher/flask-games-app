5e29e60b492f480386f04b76094b86cee308c236
lgallagher <lewisjgallagher@gmail.com>
2023-02-12 20:25:55 +0000

    :wrench: Add secret key generation and sqlite URI

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 import os
 import secrets
 
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
     CLIENT_ID = os.environ.get('CLIENT_ID')
     CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
     SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_urlsafe(16)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        f"sqlite:///{os.path.join(BASEDIR, 'app.db')}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
